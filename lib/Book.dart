e61e19a661c7cf2dea73d6b0ec7fc27e0e59ed5c
Corneliu Dascalu <corneliu.dascalu@gmail.com>
2019-03-15 12:42:23 +0200

    Create Book entity

---
+++ lib/Book.dart
@@ -0,0 +1,8 @@
+class Book {
+  final String author;
+  final String title;
+  final String description;
+  final String iconUrl;
+
+  Book({this.author, this.title, this.description, this.iconUrl});
+}
